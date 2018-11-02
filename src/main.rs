c1db44bc82f2569f790e6db5865cca040fdb4cca
Øystein Hovind <oystein.hovind@folq.no>
2018-11-02 17:31:14 +0100

    Working multiplication trait

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 extern crate nalgebra as na;
 extern crate num_traits as num;
+extern crate alga as al;
 
 use std::{f64, char, fmt};
-use num::One;
-use na::{DefaultAllocator, Scalar, Dim, DimName, VectorN};
+use std::ops::{Add, Mul};
+use na::{DefaultAllocator, Dim, Real, VectorN};
 use na::allocator::Allocator;
 use na::dimension::*;
 
 fn main() {
-   let k = Dual::<f64, U3>::new(1.0f64);
-   println!("{}", k);
+   let x = Dual::<f64, U3>::new(1.0f64);
+   println!("{}", x.idx(0) * x.idx(1) * x.idx(1) + x.idx(1));
 }
 
 //#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Dual<S: Scalar, D: Dim>
-    where DefaultAllocator: Allocator<S, D>
+pub struct Dual<S, D>
+where
+    S: Real,
+    D: Dim,
+    DefaultAllocator: Allocator<S, D>,
 {
     a: S,
     b: VectorN<S, D>,
@@ -22,21 +26,60 @@ pub struct Dual<S: Scalar, D: Dim>
 
 impl<S, D> Dual<S, D>
 where
-    S: Scalar + Copy + One,
+    S: Real,
     D: Dim + DimName,
     DefaultAllocator: Allocator<S, D>,
 {
     pub fn new(value : S) -> Dual<S, D> {
         Dual {
             a: value,
-            b: VectorN::<S, D>::from_fn(|_, _| S::one() ),
+            b: VectorN::<S, D>::from_fn(|_, _| S::one()),
+        }
+    }
+    pub fn idx(&self, index : usize) -> Dual<S, D> {
+        Dual {
+            a: self.a,
+            b: VectorN::<S, D>::from_fn(|i : usize, _| if i == index { S::one() } else { S::zero() } ),
+        }
+    }
+
+}
+
+impl<S, D> Add for Dual<S, D>
+where
+    S: Real,
+    D: Dim,
+    DefaultAllocator: Allocator<S, D>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Dual<S, D>) -> Dual<S, D> {
+        Dual {
+            a: self.a + rhs.a,
+            b: self.b + rhs.b,
+        }
+    }
+}
+
+impl<S, D> Mul for Dual<S, D>
+where
+    S: Real + Mul<VectorN<S, D>, Output=VectorN<S, D>>,
+    D: Dim,
+    DefaultAllocator: Allocator<S, D>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Dual<S, D>) -> Dual<S, D> {
+        Dual {
+            a: self.a * rhs.a,
+            b: self.a * rhs.b + rhs.a * self.b,
         }
     }
 }
 
 impl<S, D> fmt::Display for Dual<S, D>
 where
-    S: Scalar + fmt::Display,
+    S: Real + fmt::Display,
     D: Dim,
     DefaultAllocator: Allocator<S, D>,
 {
@@ -57,3 +100,22 @@ where
  * x[0] should be { x'[0] + [1.0f64 0.0f64 ..]
  *
  * */
+/*impl<S, D> Index<usize> for Dual<S, D>
+where
+    S: Scalar,
+    D: Dim,
+    DefaultAllocator: Allocator<S, D>,
+{
+    type Output = Dual<S, D>;
+
+    fn index(&self, index: usize) -> &Dual<S, D> {
+        assert!(
+            index < self.b.len(),
+            "Matrix index out of bounds."
+        );
+        &Dual {
+            a: self.a,
+            b: self.b,
+        }
+    }
+}*/
