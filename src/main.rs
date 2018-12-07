b7ac5240bfa1205acab87e95c79254e1f3dda5b0
Øystein Hovind <oystein.hovind@folq.no>
2018-12-07 13:49:11 +0100

    Lift comments

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,16 @@ use na::{DefaultAllocator, Dim, Real, VectorN};
 use na::allocator::Allocator;
 use na::dimension::*;
 
-fn f<S : Real>(x: VectorN<S, U2>) -> VectorN<S, U2>
+fn f<S : Real>(x: VectorN<S, U2>) -> VectorN<S, U3>
 {
-    VectorN::<S, U2>::new(x[0] + x[1], x[0]*x[1])
+    VectorN::<S, U3>::new(x[0] + x[1], x[0]*x[1], x[0] - x[1])
 }
 
 fn main() {
    let x = VectorN::<f64, U2>::new(1.0f64, 2.0f64);
-   let dx = Dual::<f64, U2>::new(x);
+   /*let dx = Dual::<f64, U2>::new(x);
    let y = f(dx);
-   println!("{}", y);
-   /*let x = Dual::<f64, U2>::new(1.0f64);
-   let y = Dual::<f64, U2>::new(2.0f64);
-   let v = VectorN::<Dual<f64, U2>, U2>::new(x, y);
-   println!("{}", x.idx(0) * x.idx(1) * x.idx(1) + 2.0*x.idx(1));*/
+   println!("{}", y);*/
 }
 
 #[derive(Debug, PartialEq)]
@@ -43,16 +39,16 @@ where
 {
     pub fn new(x : VectorN<S, D>) -> VectorN<Dual<S, D>, D> {
         VectorN::<S, D>::from_fn(|i : usize, _| -> Dual<S, D> {
-                Dual {
-                    a: x[i],
-                    b: VectorN::<S, D>::from_fn(|j : usize, _| -> S {
-                            if i == j {
-                                S::one()
-                            } else {
-                                S::zero()
-                            }
-                     }),
-                }
+            Dual {
+                a: x[i],
+                b: VectorN::<S, D>::from_fn(|j : usize, _| -> S {
+                    if i == j {
+                        S::one()
+                    } else {
+                        S::zero()
+                    }
+                }),
+            }
         })
     }
 
@@ -104,21 +100,6 @@ where
         }
     }
 }
-/*impl<S, D> Mul<S> for Dual<S, D>
-where
-    S: Real + Mul<VectorN<S, D>, Output=VectorN<S, D>>,
-    D: Dim,
-    DefaultAllocator: Allocator<S, D>,
-{
-    type Output = Self;
-
-    fn mul(self, rhs: S) -> Dual<S, D> {
-        Dual {
-            a: self.a * rhs,
-            b: self.b * rhs,
-        }
-    }
-}*/
 
 impl<S, D> fmt::Display for Dual<S, D>
 where
@@ -134,39 +115,3 @@ where
         Ok(())
     }
 }
-
-/*impl Real<S, D> for Dual<S, D>
-where
-    S: Real,
-    D: Dim,
-    DefaultAllocator: Allocator<S, D>,
-{
-}*/
-
-/*fn main() {
-    let x = lib::Dual::new(2.0f64);
-    println!("{}", 100.0f64 + 2.5f64*x*x + 1.0f64);
-} */
-/* let x = Container<Dual<T>>
- * x[0] should be { x'[0] + [1.0f64 0.0f64 ..]
- *
- * */
-/*impl<S, D> Index<usize> for Dual<S, D>
-where
-    S: Scalar,
-    D: Dim,
-    DefaultAllocator: Allocator<S, D>,
-{
-    type Output = Dual<S, D>;
-
-    fn index(&self, index: usize) -> &Dual<S, D> {
-        assert!(
-            index < self.b.len(),
-            "Matrix index out of bounds."
-        );
-        &Dual {
-            a: self.a,
-            b: self.b,
-        }
-    }
-}*/
